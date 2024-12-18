e6df55734fa5b1eeec847f78ad146c3dd1acf062
Rohanpatel08 <raj.bangoriya08@gmail.com>
2024-12-18 22:37:24 +0530

    push notification packages added , firebase-php and fcm

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ env('APP_NAME', 'Laravel') }}</title>
+    <title>{{ env('APP_NAME') }}</title>
 </head>
 
 <body>
