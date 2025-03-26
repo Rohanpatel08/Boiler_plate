90c56abba4e375a88939cfff8d67fcf0de070f0a
Rohanpatel08 <raj.bangoriya08@gmail.com>
2025-03-26 22:43:51 +0530

    api/api.php file added

---
+++ routes/api/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/test', function (Request $request) {
+    $users = User::all();
+    dd($users);
+});
